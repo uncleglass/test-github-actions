552735f87026a2453f8cab9987597f4ca09447dd
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-10-15 19:46:47 +0200

    feat: Initial commit

--- src/test/java/pl/uncleglass/testgithubactions/TestGithubActionsApplicationTests.java
+++ src/test/java/pl/uncleglass/testgithubactions/TestGithubActionsApplicationTests.java
@@ -3,11 +3,18 @@ package pl.uncleglass.testgithubactions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class TestGithubActionsApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void test() {
+        assertTrue(false);
     }
 
 }
